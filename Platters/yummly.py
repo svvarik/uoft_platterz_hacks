7e055cb22187a2610716b12ec72c2b5a085e3dbd
Javan Graham <javanag8@gmail.com>
2018-02-17 07:56:46 -0500

    Added yummly api support functions

---
+++ Platters/yummly.py
@@ -0,0 +1,115 @@
+import os
+import requests
+
+SEARCH_URL = 'http://api.yummly.com/v1/api/recipes'
+GET_URL = 'http://api.yummly.com/v1/api/recipe/'
+
+class Recipe:
+    """
+    Object representing a recipe
+    """
+    def __init__(self, ID, name, ingredients, ingredient_lines, size='', images=[], nutrition=[], thumbnails=[], site='', cook_time=0):
+        self.ID = ID        
+        self.name = name
+        self.ingredients = ingredients
+        self.ingredient_lines = ingredient_lines
+
+        self.size = size
+        self.images = images
+        self.thumbnails = thumbnails
+        self.site = site
+        self.nutrition = nutrition
+        self.cook_time = cook_time
+
+
+class Nutrient:
+    """
+    Model for the values of a single nutrient (e.g. protein) in a given Recipe.
+    """
+
+    def __init__(self, attribute, description, value, unit, name, abbreviation, plural, plural_abbreviation):
+        self.attribute = attribute
+        self.description = description
+        self.value = value
+        self.unit = unit
+
+        self.name = name
+        self.abbreviation = abbreviation
+        self.plural = plural
+        self.plural_abbreviation = plural_abbreviation
+
+
+def get_app_id():
+    """Return app ID from environment variable for safety."""
+    return os.environ.get('YUMMLY_APP_ID')
+
+
+def get_app_key():
+    """Return secret key from environment variable for safety."""
+    return os.environ.get('YUMMLY_KEY')
+
+
+def search_recipes(likes, dislikes, diet, allergy, courses, results, q=''):
+    """
+    Send a search request to yummly.
+
+    likes - list of strings : keywords of foods user likes
+    dislikes - list of strings : keywords of foods user dislikes
+    diet - list of strings : keywords of Edamam diet filters
+    allergy - list of strings : keywords of Edamam health filters
+    courses - list of strings : breakfast, lunch, or dinner(include brunch etc)
+    q - string : search query string
+    results - int : max number of results to request
+    """
+
+    PARAMETERS = {'q':[q], '_app_id':[get_app_id()], '_app_key':[get_app_key()], 'requirePictures':['true'], 'excludedIngredient[]':dislikes, 'allowedDiet[]':diet, 'allowedAllergy[]':allergy, 'allowedCourse[]':courses, 'maxResult':[results]}
+
+    request = requests.get(url = SEARCH_URL, params = PARAMETERS)
+    response = request.json()
+    recipes = parse_search_response(response)
+
+    return recipes
+
+
+def parse_search_response(response):
+    """Helper function to search_recipes to encapsulate JSON parsing."""
+
+    recipes = []
+    matches = response['matches']
+    for recipe in matches:
+        thumbnails = []
+        if 'smallImageUrls' in recipe.keys():
+            thumbnails = recipe['smallImageUrls']
+
+        site = ''
+        if 'sourceDisplayName' in recipe.keys():
+            site = recipe['sourceDisplayName']
+
+        cook_time = 0
+        if 'totalTimeInSeconds' in recipe.keys():
+            thumbnails = recipe['totalTimeInSeconds']
+    
+        #Get further information for nutrition and steps etc.
+        PARAMETERS = {'_app_id':[get_app_id()], '_app_key':[get_app_key()]}
+        URL = GET_URL + recipe['id']
+        get_request = requests.get(url = URL, params = PARAMETERS)
+        get_response = get_request.json()
+
+        nutrition = []
+        if 'nutritionEstimates' in recipe.keys():
+            values = get_response['nutritionEstimates']
+            for value in values:
+                nutrition.append(Nutrition(value[attribute], value[description], value[value], value[unit], value[name], value[abbreviation], value[plural], value[pluralAbbreviation]))
+
+        images = []
+        if 'images' in recipe.keys():
+            images = get_response['images']
+
+        size = ''
+        if 'yield' in recipe.keys():
+            size = get_response['yield']
+        
+        recipes.append(Recipe(recipe['id'], recipe['recipeName'], recipe['ingredients'], get_response['ingredientLines'], size, images, nutrition, thumbnails, site, cook_time))
+    return recipes
+
+
