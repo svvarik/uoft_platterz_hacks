357eac114ea530ddf528871de9ad3e50eeb84923
Runtian Wang <bettyruntian.wang@mail.utoronto.ca>
2018-02-17 16:13:29 -0500

    grocery list

--- Platters/grocery.py
+++ Platters/grocery.py
@@ -5,8 +5,8 @@ class ShoppingList:
     def __init__(self):
         self.lst = {}
         self.measurements = ['whole', 'cup', 'cups', 'tsp', 'teaspoon', 'teaspoons', 'tbsp', 'tablespoons',
-               'tablespoon', 'ounce', 'ounce', 'grams', 'pounds', 'pound' ,'lbs', 'lb',
-               'kilograms', 'stick', 'cloves', 'ml', 'millilitres', 'L', 'mL', 'litres']
+               'tablespoon', 'ounce', 'ounces', 'grams', 'pounds', 'pound' ,'lbs', 'lb',
+               'kilograms', 'stick', 'cloves', 'ml', 'millilitres', 'L', 'mL', 'litres', 'slices', 'slices']
 
     def add(self, recipe):
         if isinstance(recipe, Recipe):
@@ -28,18 +28,19 @@ class ShoppingList:
                 else:
                     cur_ingredient = recipe.ingredients[i]
                     if self.lst[cur_ingredient] != "unknown" and amount != "unknown":
-                        print(amount[1])
-                        print((self.lst[cur_ingredient].split(" "))[1])
-                        if (amount[1] == ((self.lst[cur_ingredient]).split(" "))[1]):
+                        if len(amount) >= 2 and amount[1] == (self.lst[cur_ingredient].split(" "))[1]:
                             try:
-                                value = str(int(ingredient_line_split[0]) + int(amount.split(" ")[0]))
+                                value = str(int(ingredient_line_split[0]) + int(amount[0]))
                                 self.lst[cur_ingredient] = value + " " + amount[1]
                             except ValueError:
                                 value = amount[0] + " " + amount[1]
                                 self.lst[cur_ingredient] = self.lst[cur_ingredient] + " + " + value
-                        else:
+                        elif len(amount) >= 2:
                             value = amount[0] + " " + amount[1]
                             self.lst[cur_ingredient] = self.lst[cur_ingredient] + " + " + value
+
+                        else:
+                            self.lst[cur_ingredient] = self.lst[cur_ingredient] + " + " + amount[0]
 
                     else:
                         self.lst[cur_ingredient] = "unknown"
@@ -49,16 +50,5 @@ class ShoppingList:
         return self.lst
 
 
-if __name__ == '__main__':
-    recipe = Recipe("French-Onion-Soup-The-Pioneer-Woman-Cooks-_-Ree-Drummond-41364","French Onion Soup",
-                    ["Butter", "Yellow Onions", "White Wine", "Chicken Broth", "beef Broth", "Minced Garlic",
-                     "Worcestershire Sauce", "French Bread", "Gruyere Cheese"],
-                    ["1 stick Butter", "4 whole Large (or 6 Medium) Yellow Onions, Halved Root To Tip, And Sliced Thin",
-                     "1 cup (generous) Dry White Wine", "4 cups Low Sodium Chicken Broth", "4 cups Beef Broth",
-                     "2 cloves Minced Garlic",
-                     "Worcestershire Sauce", "Several Thick Slices Of French Bread Or Baguette",
-                     "5 ounces, weight (to 7 Ounces) Gruyere Cheese, Grated"])
 
-    slst = ShoppingList()
-    print(slst.add(recipe))
 
