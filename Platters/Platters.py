0de5193803b44e8ff07e69098d500cd1baff1c1c
svvarik <sv.varikooty@mail.utoronto.ca>
2018-02-16 21:53:08 -0500

    Updated to include MongoDB Mlab connection

--- Platters/Platters.py
+++ Platters/Platters.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+MONGODB_URI = "mongodb://platters_hacks:platters@ds227168.mlab.com:27168/platters_hacks"
+client = MongoClient(MONGODB_URI)
+db = client.get_database('platters_hacks')
+
 
 
 @app.route('/')
